c5ff2d2f55bdf282c43c3a89731f2628e2b716cd
Vladyslav Bielov <68798347+vladbielov@users.noreply.github.com>
2020-10-05 12:13:32 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -41,6 +41,14 @@ variable "az3" {
   type    = string
 }
 
+#
+environment               = var.environment
+  vpc_cidr_block            = var.vpc_cidr_block
+  availability_zones        = [ var.az1, var.az2, var.az3 ]
+  public_subnet_cidr_blocks = [ var.public_cidr1, var.public_cidr2, var.public_cidr3 ]
+  private_subnet_cidr_blocks = [ var.private_cidr1, var.private_cidr2, var.private_cidr3 ]
+#
+
 variable "region" {
   type    = string
 }
