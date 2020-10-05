7f17446d064e2034b1deabbba9d41e15d2131261
Vladyslav Bielov <68798347+vladbielov@users.noreply.github.com>
2020-10-05 12:00:44 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,45 @@ variable "environment" {
   type    = string
 }
 
+variable "vpc_cidr_block" {
+  type    = string
+}
+
+variable "public_cidr1" {
+  type    = string
+}
+
+variable "public_cidr2" {
+  type    = string
+}
+variable "public_cidr3" {
+  type    = string
+}
+
+variable "private_cidr1" {
+  type    = string
+}
+
+variable "private_cidr2" {
+  type    = string
+}
+
+variable "private_cidr3" {
+  type    = string
+}
+
+variable "az1" {
+  type    = string
+}
+
+variable "az2" {
+  type    = string
+}
+
+variable "az3" {
+  type    = string
+}
+
 variable "region" {
   type    = string
 }
