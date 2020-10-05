59066dee52d62a9094ff8d70a4aec86b605566f2
Vladyslav Bielov <68798347+vladbielov@users.noreply.github.com>
2020-10-05 16:37:34 -0500

    Create sec_group.tf

---
+++ sec_group.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "vpc_task" {
+  name        = "vpc_task"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    description = "Allow ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
