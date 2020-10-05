3d769ddb06340a5d6cb2470e6c069c958dda19cb
Vladyslav Bielov <68798347+vladbielov@users.noreply.github.com>
2020-10-05 12:09:01 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_key_pair" "my-key" {
+  key_name   = var.key_name
+  public_key = var.public_key
+}
+
+
 ### Network
 
 # Internet VPC
@@ -46,8 +56,6 @@ resource "aws_internet_gateway" "internet_gw" {
   tags = {
     Name = "${var.environment}_internet_gw"
   }
-provider "aws" {
-  region = var.region
 }
 
 # route tables
@@ -62,20 +70,12 @@ resource "aws_route_table" "public_rt" {
   tags = {
     Name = "${var.environment}_public_rt"
   }
-resource "aws_key_pair" "my-key" {
-  key_name   = var.key_name
-  public_key = var.public_key
 }
 
 # NAT EIPs
 resource "aws_eip" "nat_eip" {
   vpc   = true
   count = length(aws_subnet.public_subnets)
-resource "aws_instance" "web" {
-  key_name = aws_key_pair.my-key.key_name
-  instance_type = var.instance_type
-  ami           = var.ami_id
-  security_groups = [aws_security_group.web.id]
 
   tags = {
     Name = "${var.environment}_nat_eip"
@@ -87,11 +87,9 @@ resource "aws_nat_gateway" "nat_gw" {
   allocation_id = element(aws_eip.nat_eip.*.id, count.index)
   subnet_id     = element(aws_subnet.public_subnets.*.id, count.index)
   count         = length(aws_subnet.public_subnets)
-  subnet_id     = tolist(data.aws_subnet_ids.my-subnets.ids)[0]
 
   tags = {
     Name = "${var.environment}_nat_gw"
-    Name = "${var.environment}-instance"
   }
 }
 
@@ -121,3 +119,17 @@ resource "aws_route_table_association" "public_rta" {
   subnet_id      = element(aws_subnet.public_subnets.*.id, count.index)
   route_table_id = element(aws_route_table.public_rt.*.id, count.index)
   count          = length(aws_subnet.public_subnets)
+}
+
+resource "aws_instance" "web" {
+  key_name = aws_key_pair.my-key.key_name
+  instance_type = var.instance_type
+  ami           = var.ami_id
+  security_groups = [aws_security_group.web.id]
+
+  subnet_id     = tolist(data.aws_subnet_ids.my-subnets.ids)[0]
+
+  tags = {
+    Name = "${var.environment}-instance"
+  }
+}
