e0c681c0cdd77ac044662c5f7f2ed39922d2e5ce
Vladyslav Bielov <68798347+vladbielov@users.noreply.github.com>
2020-10-05 16:56:35 -0500

    Update ngw.tf

--- ngw.tf
+++ ngw.tf
@@ -1,7 +1,6 @@
 resource "aws_nat_gateway" "ngw" {
   allocation_id = "${aws_eip.nat.id}"
   subnet_id     = "${aws_subnet.public3.id}"
-  tags          = "${var.tags}"
 }
 
 resource "aws_eip" "nat" {
