bf59ea37f60119e4ac6d825b3e44638aeba3bcf0
Vladyslav Bielov <68798347+vladbielov@users.noreply.github.com>
2020-10-05 16:38:53 -0500

    Create vpc_sec_group.tf

---
+++ vpc_sec_group.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "vpc_task" {
+  name        = "vpc_task"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    description = "Allow ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
